1912f4b6372458516dc33ba80815d75c0219a7be
cristoflop <cristofer.lc.99@gmail.com>
2021-02-07 16:09:34 +0100

    Added company repository

---
+++ src/main/java/es/urjc/cloudapps/planes/data/CompanyRepository.java
@@ -0,0 +1,9 @@
+package es.urjc.cloudapps.planes.data;
+
+import es.urjc.cloudapps.planes.domain.Company;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+}
