3631851d8fe4e9d55834a252fbe883a60b0bff68
cristoflop <cristofer.lc.99@gmail.com>
2021-02-07 23:49:53 +0100

    Refactor PK in airport repository

--- src/main/java/es/urjc/cloudapps/planes/data/AirportRepository.java
+++ src/main/java/es/urjc/cloudapps/planes/data/AirportRepository.java
@@ -1,11 +1,12 @@
 package es.urjc.cloudapps.planes.data;
 
 import es.urjc.cloudapps.planes.domain.Airport;
+import es.urjc.cloudapps.planes.domain.Iata;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AirportRepository extends CrudRepository<Airport, Long> {
+public interface AirportRepository extends CrudRepository<Airport, Iata> {
 
     Airport findByName(String name);
 
