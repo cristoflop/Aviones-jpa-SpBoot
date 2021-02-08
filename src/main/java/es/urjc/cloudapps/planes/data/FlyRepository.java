3c854b841b632c845ea60e98b1fae40a5405fac8
cristoflop <cristofer.lc.99@gmail.com>
2021-02-08 17:31:05 +0100

    Added query2: get flies of destination airport in date order by date

--- src/main/java/es/urjc/cloudapps/planes/data/FlyRepository.java
+++ src/main/java/es/urjc/cloudapps/planes/data/FlyRepository.java
@@ -1,9 +1,19 @@
 package es.urjc.cloudapps.planes.data;
 
 import es.urjc.cloudapps.planes.domain.Fly;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
 
 @Repository
 public interface FlyRepository extends CrudRepository<Fly, String> {
+
+    @Query("from Fly fly where fly.destination.city = ?1 and fly.startTime = ?2 order by fly.startTime")
+    List<Fly> findAllByDestinationCityAfterDate(@Param("city") String city,
+                                                @Param("date") Date date);
+
 }
